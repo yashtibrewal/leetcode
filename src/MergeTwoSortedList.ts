9419be875f4c695af91ea6b2202837f280170ca9
Yash <yashkush.tibrewal@gmail.com>
2020-09-05 00:03:12 +0530

    refactored merge two sort

--- src/MergeTwoSortedList.ts
+++ src/MergeTwoSortedList.ts
@@ -1,58 +1,72 @@
-import { it } from "mocha";
-
+/**
+ * class structure for a linked list node
+ */
 export class ListNode {
     val: number
     next: ListNode | null
+    /**
+     *
+     * @param {number} val
+     * @param {ListNode | null} next
+     */
     constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
+      this.val = (val === undefined ? 0 : val);
+      this.next = (next === undefined ? null : next);
     }
 }
-function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    // checking for null inputs
-    if(l1 === null){
-        if(l2 === null){
-            return null;
-        }
-        return l2;
-    }else if(l2 === null){
-        return l1;
+/**
+ * Join 2 sorted list
+ * @param {ListNode | null} l1
+ * @param {ListNode | null} l2
+ * @return {ListNode | null}
+ */
+function mergeTwoLists(
+    l1: ListNode | null,
+    l2: ListNode | null): ListNode | null {
+  // checking for null inputs
+  if (l1 === null) {
+    if (l2 === null) {
+      return null;
     }
+    return l2;
+  } else if (l2 === null) {
+    return l1;
+  }
 
-    // creating list nodes for future use
-    let head: ListNode;
-    let iterator: ListNode = new ListNode();
-    
-    // initializing the first head node of result
-    if(l1.val<l2.val){
-        iterator = l1;
-        l1 = l1.next;
-    }else{
-        iterator = l2;
-        l2 = l2.next;
-    }
-    head = iterator;
+  // creating list nodes for future use
 
-    // traversing through both input lists
-    while(l1!==null && l2!== null){
-        if(l1.val<l2.val){
-            iterator.next = l1;
-            l1 = l1.next;
-        }else{
-            iterator.next = l2;
-            l2 = l2.next;
-        }
-        iterator = iterator.next;
-        iterator.next = null;
-    }
+  let iterator: ListNode = new ListNode();
 
-    // combined the left of list 
-    if(l1===null){
-        iterator.next = l2;
-    }else if(l2 === null){
-        iterator.next = l1;
+  // initializing the first head node of result
+  if (l1.val<l2.val) {
+    iterator = l1;
+    l1 = l1.next;
+  } else {
+    iterator = l2;
+    l2 = l2.next;
+  }
+  const head: ListNode = iterator;
+
+  // traversing through both input lists
+  while (l1!==null && l2!== null) {
+    if (l1.val<l2.val) {
+      iterator.next = l1;
+      l1 = l1.next;
+    } else {
+      iterator.next = l2;
+      l2 = l2.next;
     }
-    return head;
+    iterator = iterator.next;
+    iterator.next = null;
+  }
+
+  // combined the left of list
+  if (l1===null) {
+    iterator.next = l2;
+  } else if (l2 === null) {
+    iterator.next = l1;
+  }
+  return head;
 };
 
 export default mergeTwoLists;
--- test/MergeTwoSortedList.spec.ts
+++ test/MergeTwoSortedList.spec.ts
@@ -1,56 +1,54 @@
-import "mocha";
-import {expect} from "chai";
-import MergeTwoSortedList from '../src/MergeTwoSortedList';
+import 'mocha';
+import {expect} from 'chai';
+import mergeTwoLists from '../src/MergeTwoSortedList';
 import {ListNode} from '../src/MergeTwoSortedList';
 
-describe("Testing MergeTwoSortedList", () => {
+describe('Testing MergeTwoSortedList', () => {
+  it('should return null', ()=>{
+    const result:any = mergeTwoLists(null, null);
+    expect(result).to.equal(null);
+  });
 
-    it("should return null",()=>{
-        const result:any = MergeTwoSortedList(null,null);
-        expect(result).to.equal(null);
-    });
+  it('should return param1', ()=>{
+    const param1 = new ListNode(10, new ListNode(15));
+    const result = mergeTwoLists(param1, null);
+    expect(result).to.equal(param1);
+  });
 
-    it('should return param1',()=>{
-        const param1 = new ListNode(10, new ListNode(15));
-        const result = MergeTwoSortedList(param1,null);
-        expect(result).to.equal(param1);
-    });
+  it('should return param2', ()=>{
+    const param1 = new ListNode(10, new ListNode(15));
+    const result = mergeTwoLists(null, param1);
+    expect(result).to.equal(param1);
+  });
 
-    it('should return param2',()=>{
-        const param1 = new ListNode(10, new ListNode(15));
-        const result = MergeTwoSortedList(null,param1);
-        expect(result).to.equal(param1);
-    });
+  it('should return combined listnodes', ()=>{
+    const l1 = new ListNode(10, null);
+    const l2 = new ListNode(12, null);
+    const l3 = new ListNode(15, null);
+    const l4 = new ListNode(20, null);
+    l1.next = l3;
+    l2.next = l4;
+    const result = mergeTwoLists(l1, l2);
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = l4;
+    expect(result).to.equal(l1);
+  });
 
-    it('should return combined listnodes',()=>{
-        const l1 = new ListNode(10, null);
-        const l2 = new ListNode(12, null);
-        const l3 = new ListNode(15, null);
-        const l4 = new ListNode(20, null);
-        l1.next = l3;
-        l2.next = l4;
-        const result = MergeTwoSortedList(l1,l2);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        expect(result).to.equal(l1);
-    });
-
-    it('should return combined listnodes',()=>{
-        const l1 = new ListNode(10, null);
-        const l2 = new ListNode(12, null);
-        const l3 = new ListNode(15, null);
-        const l4 = new ListNode(20, null);
-        const l5 = new ListNode(20, null);
-        l1.next = l3;
-        l2.next = l4;
-        l4.next = l5;
-        const result = MergeTwoSortedList(l1,l2);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        expect(result).to.equal(l1);
-    });
-
+  it('should return combined listnodes', ()=>{
+    const l1 = new ListNode(10, null);
+    const l2 = new ListNode(12, null);
+    const l3 = new ListNode(15, null);
+    const l4 = new ListNode(20, null);
+    const l5 = new ListNode(20, null);
+    l1.next = l3;
+    l2.next = l4;
+    l4.next = l5;
+    const result = mergeTwoLists(l1, l2);
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = l4;
+    l4.next = l5;
+    expect(result).to.equal(l1);
+  });
 });
